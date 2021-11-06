14138c0b99a5e08c62801f0453d3116f20d4233a
Denver <70090559+Denver44@users.noreply.github.com>
2021-11-06 21:48:00 +0530

    Home automation code using esp8266 board

---
+++ HomeAutomation-ESP8266/Ep-1_ESPAsyncWebServer_mDNS.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESPAsyncWebServer.h>
+AsyncWebServer server(80); // server port 80
+
+void notFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "text/plain", "Page Not found");
+}
+
+void setup(void)
+{
+  
+  Serial.begin(115200);
+  
+  WiFi.softAP("6628PSE", "987654321");
+  Serial.println("softap");
+  Serial.println("");
+  Serial.println(WiFi.softAPIP());
+
+
+  if (MDNS.begin("ESP")) { //esp.local/
+    Serial.println("MDNS responder started");
+  }
+
+
+
+  server.on("/", [](AsyncWebServerRequest * request)
+  { 
+   String message = "hello world"; 
+  request->send(200, "text/plain", message);
+  });
+
+   server.on("/page1", HTTP_GET, [](AsyncWebServerRequest * request)
+  { 
+   String message = "Welcome to page1"; 
+  request->send(200, "text/plain", message);
+  });
+
+  server.onNotFound(notFound);
+
+  server.begin();  // it will start webserver
+}
+
+
+void loop(void)
+{
+}
